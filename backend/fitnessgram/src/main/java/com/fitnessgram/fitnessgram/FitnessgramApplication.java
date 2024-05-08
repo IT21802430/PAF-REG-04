36a48a29c8a68ce26ea44f4ef1acf2182404640f
Padmatilaka R W <110711978+IT21802430@users.noreply.github.com>
2024-05-08 23:05:07 +0530

    Springboot build

---
+++ backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/FitnessgramApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessgram.fitnessgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessgramApplication.class, args);
+	}
+
+}
