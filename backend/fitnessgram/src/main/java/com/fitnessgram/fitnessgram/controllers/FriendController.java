cb54b33ab50c2c4d45dce1916eb21751f0228ef5
Padmatilaka R W <110711978+IT21802430@users.noreply.github.com>
2024-05-09 14:49:54 +0530

    Controllers code

--- backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/controllers/FriendController.java
+++ backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/controllers/FriendController.java
@@ -1,5 +1,20 @@
 package com.fitnessgram.fitnessgram.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class FriendController {
     
+    @Autowired
+    private FriendService friendService;
+
+    @GetMapping("friend/{friendId}/add")
+    public ApiResponse saveFriendByUser(Principal principal, @PathVariable Integer friendId) {
+        this.friendService.addFriend(principal.getName(), friendId);
+        return new ApiResponse("Friend is Successfully Added !!", true);
+    }
 }
--- backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/controllers/LikeController.java
+++ backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/controllers/LikeController.java
@@ -1,5 +1,42 @@
 package com.fitnessgram.fitnessgram.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class LikeController {
     
+	@Autowired
+	private LikeService likeService;
+
+	@GetMapping("/post/{postId}/like")
+	public ResponseEntity<LikeDto> createLike(@RequestBody LikeDto like,
+			@PathVariable Integer postId,
+			Principal principal) {
+
+		LikeDto createLike = this.likeService.createLike(like, postId, principal.getName());
+		return new ResponseEntity<LikeDto>(createLike, HttpStatus.CREATED);
+	}
+
+	@PostMapping("/post/{postId}/user/{userId}/like")
+	public ResponseEntity<LikeDto> createLike(@RequestBody LikeDto like,
+			@PathVariable Integer postId,
+			@PathVariable Integer userId) {
+
+		LikeDto createLike = this.likeService.createLike(like, postId, userId);
+		return new ResponseEntity<LikeDto>(createLike, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/likes/{likeId}")
+	public ResponseEntity<ApiResponse> deleteLike(@PathVariable Integer likeId) {
+
+		this.likeService.deleteLike(likeId);
+
+		return new ResponseEntity<ApiResponse>(new ApiResponse("Like deleted successfully !!", true), HttpStatus.OK);
+	}
 }
