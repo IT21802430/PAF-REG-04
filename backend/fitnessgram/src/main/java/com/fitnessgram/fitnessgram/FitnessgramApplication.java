a5a4f5b6b333f14a259eae48030f510519840a2a
it21803352 <it21803352@my.sliit.lk>
2024-05-10 00:39:35 +0530

    main function updated

--- backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/FitnessgramApplication.java
+++ backend/fitnessgram/src/main/java/com/fitnessgram/fitnessgram/FitnessgramApplication.java
@@ -6,8 +6,54 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitnessgramApplication {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private RoleRepo roleRepo;
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessgramApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(this.passwordEncoder.encode("xyz"));
+
+		try {
+
+			Role role = new Role();
+			role.setId(AppConstants.ADMIN_USER);
+			role.setName("ROLE_ADMIN");
+
+			Role role1 = new Role();
+			role1.setId(AppConstants.NORMAL_USER);
+			role1.setName("ROLE_NORMAL");
+
+			List<Role> roles = List.of(role, role1);
+
+			List<Role> result = this.roleRepo.saveAll(roles);
+
+			result.forEach(r -> {
+				System.out.println(r.getName());
+			});
+
+			// userRepo.save(user);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
 	}
 
 }
